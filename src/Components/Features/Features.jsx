3e10c400b3c2129165c88444b391a15533e441a2
Kartik Bhadouriya <ks8783375@gmail.com>
2025-06-13 18:48:23 +0530

    Update Features.jsx

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -12,7 +12,8 @@ import Feature5 from '/src/assets/table/metal/metal_table1.png';
 import Feature6 from '/src/assets/Features/product_5.png';
 import Feature7 from '/src/assets/Barstool/stool6.png';
 import Feature8 from '/src/assets/table/terrazzo/terrazzo_table4.png';
-import Feature4 from '/src/assets/Features/product_5.png';
+import Feature4 from '../../assets/Features/product_5.png';
+
 
 
 const Card = ({ image, title, status }) => (
